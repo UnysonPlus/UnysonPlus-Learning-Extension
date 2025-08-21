ece4989d12b9ecbc71def7ac9da8100868184d42
jonmlas <jonmlas@gmail.com>
2025-08-21 18:56:57 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,22 +1,30 @@
-<?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
-}
+<?php if (!defined('FW')) die('Forbidden');
 
 $manifest = array();
 
 $manifest['name']        = __( 'Learning', 'fw' );
+$manifest['slug']        = 'unysonplus-learning';
 $manifest['description'] = __(
-	'This extension adds a Learning module to your theme.'
-	.' Using this extension you can add courses, lessons and tests for your users to take.',
+	'This extension adds a Learning module to your theme. '
+	. 'Using this extension you can add courses, lessons, and tests for your users to take.',
 	'fw'
 );
+
 $manifest['version']     = '1.0.12';
 $manifest['display']     = true;
 $manifest['standalone']  = true;
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Learning-Extension';
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Learning-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Learning-Extension';
+$manifest['github_branch'] = 'master';
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Learning-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/learning/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
